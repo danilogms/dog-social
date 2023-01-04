53db31bffe4dda6624e1210bda093e0d38e22166
danilogms <Danmil-20@hotmail.com>
2023-01-04 18:00:28 -0300

    Button, input and login form components are added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from 'react';
+import Api from './api/Api';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
-  return <div className="App">App React</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
