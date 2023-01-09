e93151cbca26ca9238c0e2b4f17f6206fd7b197b
danilogms <Danmil-20@hotmail.com>
2023-01-09 16:06:01 -0300

    added manuMobile in Userpage

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 
 const User = () => {
   return (
-    <section>
-      <h1 className="title">Pagina do Usuário</h1>
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='postar' element={<UserPhotoPost />} />
+        <Route path='estatistica' element={<UserStats/>} />
+      </Routes>
     </section>
   );
 };
