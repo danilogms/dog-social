84a63b79413261cdb8890aad9ad0928e954da3cd
danilogms <Danmil-20@hotmail.com>
2023-01-10 18:58:22 -0300

    FeedModal and FeedPhotos are stylized.

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -24,7 +24,7 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotoItens photo={photo} key={photo.id} />
+          <FeedPhotoItens photo={photo} key={photo.id} setModalPhoto={setModalPhoto}/>
         ))}
       </ul>
     );
