a21df40be7f7cf088bd13d300d84cfe1e523a1e3
danilogms <Danmil-20@hotmail.com>
2023-01-16 23:19:08 -0300

    Footer componnent att

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>Dogs. Alguns direitos reservados.</p>
+      <Dogs />
+      <p>Desenvolvido por Danilo Gomes</p>
+    </footer>
+  );
 };
 
 export default Footer;
